8f018cdbaca13e12f6b593757972f64213d1600e
Muhammad Muzammil Loya <73660510+Muzammil-cyber@users.noreply.github.com>
2025-05-13 23:59:32 +0500

    add: strip section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,27 @@
 import FeaturesSection from "@/components/home/features";
 import Hero from "@/components/home/hero";
+import Strip from "@/components/strip";
+import { Heading3 } from "@/components/typography/heading3";
+
+import FacebookImage from "@/assets/facebook.svg";
+import InstagramImage from "@/assets/instagram.svg";
+import SnapchatImage from "@/assets/snapchat.svg";
+import Image from "next/image";
+
+
+const SOCIAL_LINKS = [{
+  name: "facebook",
+  url: "https://facebook.com",
+  image: FacebookImage
+}, {
+  name: "Instagram",
+  url: "https://instagram.com",
+  image: InstagramImage
+}, {
+  name: "Snapchat",
+  url: "https://snapchat.com",
+  image: SnapchatImage
+}]
 
 
 export default function Home() {
@@ -7,6 +29,22 @@ export default function Home() {
     <main className="min-h-screen bg-white-200">
       <Hero />
       <FeaturesSection />
+      <Strip>
+        <Heading3 className="text-center text-muted-200 max-w-5xl">
+          At Palomino, we believe in the power of photography to connect people and capture the beauty of the world around us
+        </Heading3>
+        <div className="px-0.5 flex flex-col justify-center items-center gap-12">
+          <div className="inline-flex justify-start items-center gap-9 overflow-hidden">
+            {SOCIAL_LINKS.map((link) => (
+              <a key={link.name} href={link.url} target="_blank" className="w-12 h-12  flex justify-center items-center">
+                <Image unoptimized src={link.image} width={30} height={30} alt="" className="w-12 h-12" />
+              </a>
+            ))}
+          </div>
+          <Heading3 className="text-center text-muted-200 max-w-5xl">Follow us and become part of the Revolution</Heading3>
+        </div>
+      </Strip>
+      <div className="h-screen"></div>
     </main>
   );
 }
---
+++ src/components/strip.tsx
@@ -0,0 +1,42 @@
+'use client'
+import { cn } from "@/libs/utils";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import React from "react";
+import { useRef } from "react";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+
+interface StripProps {
+    children: React.ReactNode;
+    className?: string;
+}
+
+export default function Strip({ className, children }: StripProps) {
+
+    const ref = useRef<HTMLDivElement>(null);
+
+    useGSAP(() => {
+        gsap.registerPlugin(ScrollTrigger);
+        gsap.from(ref.current, {
+            height: 0,
+            ease: 'sine.out',
+            duration: 0.8,
+            scrollTrigger: {
+                trigger: ref.current,
+                start: "top 50%",
+                end: "bottom 50%",
+                markers: true,
+            }
+        })
+    }, {
+        scope: ref,
+    })
+
+    return (
+        <section ref={ref} className={cn("mx-auto bg-white overflow-hidden w-full", className)}>
+            <div className="px-6 py-9 flex flex-col justify-center items-center gap-12">
+                {children}
+            </div>
+        </section>
+    )
+}
